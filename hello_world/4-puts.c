b1a156261ea42ccba1f0864e5306303c4bc3790d
bisoukeiko <87bisou@gmail.com>
2024-06-17 14:13:15 +0200

    4-puts.c up4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 
+/**
+ * 4-puts.c - Write a C program that prints exactly
+ * "Programming is like building a multilingual puzzle, followed by a new line.
+ * Return: value 0
+ */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-
 	return (0);
 }
