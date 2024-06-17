25415d4d4ef6363766894cd0f6cd3f3b532650f6
bisoukeiko <87bisou@gmail.com>
2024-06-17 14:33:49 +0200

    4-puts.c up7

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- * 4-puts.c - Write a C program that prints exactly
- * "Programming is like building a multilingual puzzle
- * Return: value 0
+ * main - Write a C program that prints exactly
+ * Programming is like building a multilingual puzzle
+ * Return: 0
  */
 
 int main(void)
