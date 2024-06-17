8079323e558675e6179420e186bb03b8c7a1d1da
bisoukeiko <87bisou@gmail.com>
2024-06-17 15:49:58 +0200

    6-size.c up2

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -6,6 +6,7 @@
 */
 
 int main(void)
+{
 	printf("Size of a char: %lu byte(s)\n", sizeof(char));
 	printf("Size of an int: %lu byte(s)\n", sizeof(int));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
@@ -13,4 +14,4 @@ int main(void)
 	printf("Size of a float: %lu byte(s)\n", sizeof(float));
 
 	return (0);
-{
+}
