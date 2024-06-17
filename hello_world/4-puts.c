8c633af0326f607e403e93071cfe3395523988bb
bisoukeiko <87bisou@gmail.com>
2024-06-17 14:21:05 +0200

    4-puts.c up5

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -5,7 +5,6 @@
  * "Programming is like building a multilingual puzzle, followed by a new line.
  * Return: value 0
  */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
