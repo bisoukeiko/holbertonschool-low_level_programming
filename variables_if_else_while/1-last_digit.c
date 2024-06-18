2feb439d01b4f8540f650101848fd954ec511650
bisoukeiko <87bisou@gmail.com>
2024-06-18 12:01:31 +0200

    1-last_digit.c up6

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -25,12 +25,12 @@ int main(void)
 
 	}
 	else if(last_digit == 0)
-
+	{
 		printf("Last digit of %d is %d and is 0\n", n, last_digit);
 
 	}
 	else if(last_digit < 6)
-
+	{
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_digit);
 	}
 
