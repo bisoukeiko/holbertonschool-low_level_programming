3d28a21455eb3acf15e7b25c867048d7748216be
bisoukeiko <87bisou@gmail.com>
2024-06-18 12:05:53 +0200

    1-last_digit.c up7

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -24,14 +24,15 @@ int main(void)
 		printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
 
 	}
-	else if(last_digit == 0)
+	else if (last_digit == 0)
 	{
 		printf("Last digit of %d is %d and is 0\n", n, last_digit);
 
 	}
-	else if(last_digit < 6)
+	else if (last_digit < 6)
 	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_digit);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n",
+			n, last_digit);
 	}
 
 	return (0);
