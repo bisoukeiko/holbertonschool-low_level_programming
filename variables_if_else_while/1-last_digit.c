c4c01e63cb8db82da44cc27064ec3161f6f59812
bisoukeiko <87bisou@gmail.com>
2024-06-18 11:53:57 +0200

    1-last_digit.c up3

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -9,14 +9,14 @@
 
 int main(void)
 {
-	int n;
+	int n, last_digit;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
 	/* your code goes there */
 
-	int last_digit = n % 10;
+	last_digit = n % 10;
 
 	if (last_digit > 5)
 	{
