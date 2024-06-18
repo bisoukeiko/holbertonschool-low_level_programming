7040f84e284efbed24db3b91ce9d22a40395c657
bisoukeiko <87bisou@gmail.com>
2024-06-18 10:12:05 +0200

    0-positive_or_negative.c add

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,6 +1,6 @@
 #include <stdlib.h>
 #include <time.h>
-#includen<stdio.h>
+#include <stdio.h>
 
 /**
  * main - print whether the number stored in the variable n is
@@ -16,12 +16,17 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	if (n > 0) {
-		printf("%d is positive \n",n);
-	} else if (n < 0) {
-		printf("%d is negative \n",n);
-	} else {
-		printf("%d is zero \n",n);
+	if (n > 0)
+	{
+		printf("%d is positive \n", n);
+	}
+	else if (n < 0)
+	{
+		printf("%d is negative \n", n);
+	}
+	else
+	{
+		printf("%d is zero \n", n);
 	}
 
 	return (0);
