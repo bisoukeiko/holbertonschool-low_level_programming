69646aa560aefe47efb957e2060ccfe9ce01c5f4
bisoukeiko <87bisou@gmail.com>
2024-06-19 14:49:09 +0200

    9-print_comb.c add

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+* main -  prints all possible combinations of single-digit numbers.
+*         Numbers must be separated by ,, followed by a space
+*         Numbers should be printed in ascending order
+*         You can only use the putchar function
+* Return: value 0
+*/
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 9; num++)
+	{
+		putchar(num + '0');
+		putchar(',');
+		putchar(' ');
+	}
+
+	if (num == 9)
+	{
+		putchar(num + '0');
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
