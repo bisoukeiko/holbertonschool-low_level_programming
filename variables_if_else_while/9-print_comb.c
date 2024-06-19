63e0c9f37734ff6e447a5ce21c4e3058623b03f1
bisoukeiko <87bisou@gmail.com>
2024-06-19 14:52:45 +0200

    9-print_comb.c up

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -12,18 +12,16 @@ int main(void)
 {
 	int num;
 
-	for (num = 0; num < 9; num++)
+	for (num = 0; num <= 9; num++)
 	{
 		putchar(num + '0');
-		putchar(',');
-		putchar(' ');
-	}
 
-	if (num == 9)
-	{
-		putchar(num + '0');
+		if (num < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
-
 	putchar('\n');
 
 	return (0);
