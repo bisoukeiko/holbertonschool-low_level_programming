54d2b5c8de0b182be72a1a543de6b1377ea9e8f3
bisoukeiko <87bisou@gmail.com>
2024-06-20 14:43:13 +0200

    main.h 4-isalpha.c add

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * _islower - check for alphabetic character
+ * @c: The character to be checked
+ * Return: 1 if a letter, lowercase or uppercase
+ *         0 otherwise
+ */
+
+int _isalpha(int c)
+{
+	if (isalpha(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
