d3a8c093b6825685f83d1242e5ec2a4365dda07d
bisoukeiko <87bisou@gmail.com>
2024-06-20 14:57:59 +0200

    main.h 5-sign.c add

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _print_sign - print the sign of a number
+ * @c: The number to be checked
+ * Return: 1 if the number is greater than zero
+ *         0 if the number is zero
+ *        -1 if the number is less than zero
+ */
+
+int _print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+')
+		return (1);
+	}
+	else if (n == 0 )
+	{
+		_putchar('0')
+		return (0);
+	}
+	else if (n < 0 )
+	{
+		_putchar('-')
+		return (-1);
+	}
+}
