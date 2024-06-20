d6042301a9302d8ade36f202e0726c98052ec856
bisoukeiko <87bisou@gmail.com>
2024-06-20 13:49:52 +0200

    main.h 3-islower.c add

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _islower - check for lowercase character
+ * Return: 1 if c is lowercase, 0 othercase
+ */
+
+int _islower(int c)
+{
+	if (islower(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
