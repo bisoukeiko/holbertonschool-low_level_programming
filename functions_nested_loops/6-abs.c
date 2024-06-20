0d9cf79ff5a21a18d8e2eb55be94c97cc57063da
bisoukeiko <87bisou@gmail.com>
2024-06-20 15:48:36 +0200

    main.h 6-abs.c add

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stlib.h>
+
+/**
+ * _islower - check for lowercase character
+ * @c: The character to be checked
+ * Return: 1 if c is lowercase, 0 othercase
+ */
+
+int _abs(int num)
+{
+	return (abs(num));
+}
