e3dcbb125da27dc439ee3c04311110c62a7d7005
bisoukeiko <87bisou@gmail.com>
2024-06-20 11:27:43 +0200

    main.h 1-alphabet.c up4

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,18 +1,10 @@
 #include "main.h"
 
 /**
- * main - prints the alphabet, in lowercase,
- *        followed by a new line.
- * Return: Always 0.
+ * print_alphabet - prints the alphabet, in lowercase,
+ *                  followed by a new line.
+ * Return: nothing
  */
-
-int main(void)
-{
-
-	print_alphabet();
-
-	return (0);
-}
 
 void print_alphabet(void)
 {
