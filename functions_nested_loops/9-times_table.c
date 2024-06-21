3e9b51d1dec8d805502df4f370e6cab5fd51d7c1
bisoukeiko <87bisou@gmail.com>
2024-06-21 11:04:02 +0200

    9-times_table.c up

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -10,14 +10,16 @@ void times_table(void)
 {
 	int num1;
 	int num2;
+	int result;
 
 	for (num1 = 0; num1 <= 9; num1++)
 	{
 
 		for (num2 = 0; num2 <= 9; num2++)
 		{
-
-			_putchar(num1 * num2 + '0');
+			result = num1 * num2
+			_putchar((result / 10) + '0');
+			_putchar((result % 10) + '0');
 			_putchar(',');
 			_putchar(' ');
 
