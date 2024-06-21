61e9fc05402f4ffbc5b02ec237f894f779fea2fd
bisoukeiko <87bisou@gmail.com>
2024-06-21 14:35:25 +0200

    100-print_comb3.c add

---
+++ variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/**
+* main -  print all possible different combinations of two digits
+* Return: nothing 
+*/
+
+int main(void)
+{
+	int num1;
+	int num2;
+
+	for (num1 = 0; num1 < 9; num1++)
+	{
+
+		for (num2 = 0; num2 <=9; num++)
+		{
+
+			if(num1 < num2)
+			{
+				if (num1 != num2)
+				{
+					putchar(num1 + '0');
+					putchar(num2 + '0');
+                                }
+
+				if (num1 < 9 || num2 < 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+ 
+			}
+
+		}
+
+
+	}
+
+
+}
