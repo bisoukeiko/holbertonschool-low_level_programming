bb2f97260a56a5260b6df545b996d41b4ad8f50f
bisoukeiko <87bisou@gmail.com>
2024-06-21 06:35:20 +0200

    7-print_last_digit.c up5

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -11,15 +11,8 @@ int print_last_digit(int num)
 	int last_digit;
 	last_digit = num % 10;
 
-	if (last_digit < 0)
-	{
 
-		_putchar(last_digit);
-	}
-	else
-	{
-		_putchar(last_digit);
-	}
+	_putchar(last_digit + '0');
 
 	return (last_digit);
 }
