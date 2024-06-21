52c2efd1e6147b8841337aba6220f7c453c6faa7
bisoukeiko <87bisou@gmail.com>
2024-06-21 06:39:23 +0200

    7-print_last_digit.c up6

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -2,13 +2,14 @@
 
 /**
  * print_last_digit - print the last digit of a number
- * @last_digit: The number to be checked
+ * @num: number to be checked
  * Return: value of the last digit
  */
 
 int print_last_digit(int num)
 {
 	int last_digit;
+
 	last_digit = num % 10;
 
 	if (last_digit < 0)
