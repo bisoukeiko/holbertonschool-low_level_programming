93666f77152ca5fdfe012dc44c126f00c2ddde1a
bisoukeiko <87bisou@gmail.com>
2024-06-21 06:17:58 +0200

    7-print_last_digit.c main.h add

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print the last digit of a number
+ * @num: The number to be checked
+ * Return: value of the last digit
+ */
+
+int print_last_digit(int num)
+{
+
+	num = num % 10;
+
+	return (num);
+}
