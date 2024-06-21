96e1415d79ef7b8e2f8740bb1e5cff48b6448520
bisoukeiko <87bisou@gmail.com>
2024-06-21 13:36:10 +0200

    main.h 11-print_to_98.c add

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * print_to_98 - print all natural numbers from n to 98
+ *               followed by a new line
+ * @n: integer
+ * Return: nothing
+ */
+
+void print_to_98(int n)
+{
+
+	int cnt;
+
+	if (n > 98)
+	{
+		for (cnt = n; cnt >= 98; cnt--)
+		{
+			printf("%d", cnt);
+
+			if (cnt != 98)
+			{
+				printf(',');
+				printf(' ');
+			}
+		}
+	}
+	else
+	{
+		for (cnt = n; cnt <= 98; cnt++)
+		{
+			printf("%d", cnt);
+
+			if (cnt != 98)
+			{
+				printf(',');
+				printf(' ');
+			}
+		}
+	}
+
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,4 +1,4 @@
-int _putchar (char cha);
+int _putchar(char cha);
 
 void print_alphabet(void);
 
@@ -19,3 +19,5 @@ void jack_bauer(void);
 void times_table(void);
 
 int add(int, int);
+
+void print_to_98(int n);
