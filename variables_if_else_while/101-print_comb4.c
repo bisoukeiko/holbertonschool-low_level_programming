7fa841294f1958262078942bb3552345bbffcf6b
bisoukeiko <87bisou@gmail.com>
2024-06-22 07:10:10 +0200

    101-print_comb4.c up2

--- variables_if_else_while/101-print_comb4.c
+++ variables_if_else_while/101-print_comb4.c
@@ -13,42 +13,31 @@ int main(void)
 
 	for (num1 = 0; num1 < 9; num1++)
 	{
-
 		for (num2 = 0; num2 <= 9; num2++)
 		{
-
-			if (num1 < num2 && num1 != num2)
+			for (num3 = 0; num3 <= 9; num3++)
 			{
-
-				for (num3 = 0; num3 <=9; num3++)
+				if (num1 < num2 && num1 != num2 &&
+				    num2 < num3 && num2 != num3 &&
+				    num1 < num3 && num1 != num3)
 				{
+					putchar(num1 + '0');
+					putchar(num2 + '0');
+					putchar(num3 + '0');
 
-					if (num2 < num3 && num2 != num3 &&
-					    num1 < num3 && num1 != num3)
+					if (num1 == 7 && num2 == 8 && num3 == 9)
 					{
-						putchar(num1 + '0');
-						putchar(num2 + '0');
-						putchar(num3 + '0');
-
-						if (num1 == 7 && num2 == 8 && num3 == 9)
-						{
-							continue;
-						}
-						else
-						{
-							putchar(',');
-							putchar(' ');
-						}
+						continue;
+					}
+					else
+					{
+						putchar(',');
+						putchar(' ');
 					}
 				}
 			}
-
 		}
-
 	}
-
 	putchar('\n');
-
 	return (0);
-
 }
