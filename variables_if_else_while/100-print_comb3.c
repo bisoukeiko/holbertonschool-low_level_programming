6556a556ca5d2af37eeeeb612599427db857fe33
bisoukeiko <87bisou@gmail.com>
2024-06-22 06:15:55 +0200

    100-print_comb3.c up9

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -16,7 +16,7 @@ int main(void)
 		for (num2 = 0; num2 <= 9; num2++)
 		{
 
-			if (num1 < num2 || num1 != num2))
+			if (num1 < num2 || num1 != num2)
 			{
 				putchar(num1 + '0');
 				putchar(num2 + '0');
