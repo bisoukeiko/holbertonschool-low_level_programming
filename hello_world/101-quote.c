8f0ac96f1ff30c3bad5a521ce95bd991a20616d6
bisoukeiko <87bisou@gmail.com>
2024-06-23 07:29:35 +0200

    101-quote.c up

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -7,9 +7,8 @@
 
 int main(void)
 {
-	fprintf(stderr,
-		"and that piece of art is useful\" - Dora Korpar,
-		 2015-10-19\n");
+	write(2,
+	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
 
 	return (1);
 }
