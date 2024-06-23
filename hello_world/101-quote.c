83e9fb948a5c22287d6b1d42552adb0bf2b8e4dc
bisoukeiko <87bisou@gmail.com>
2024-06-23 07:17:56 +0200

    101-quote.c add

---
+++ hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - print the standard error
+ * Return: 1
+ */
+
+int main(void)
+{
+	fprintf(stderr,
+		"and that piece of art is useful\" - Dora Korpar,
+		 2015-10-19\n");
+
+	return (1);
+}
