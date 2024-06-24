5fea72d2316622f51f96a894dc43d9bcf60d2afe
bisoukeiko <87bisou@gmail.com>
2024-06-24 12:01:41 +0200

    5-more_numbers.c up

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * print_more_numbers - print 10 times the numbers, from 0 to 14
+ * more_numbers - print 10 times the numbers, from 0 to 14
  *                      followed by a new line
  * Return: nothing
  */
 
-void print_more_numbers(void)
+void more_numbers(void)
 {
 	int cnt;
 	int num;
