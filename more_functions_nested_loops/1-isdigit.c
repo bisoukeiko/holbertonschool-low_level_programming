b04669c342824e66d17bd7d485c2202889a9ab7d
bisoukeiko <87bisou@gmail.com>
2024-06-24 11:11:05 +0200

    main.h 1-isdigit.c add

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,22 @@
+/**
+ * _isdigit - check for a digit(0 through 9)
+ * @c: number to be checked
+ * Return: 1 c is a digit
+ *         0 otherwise
+ */
+
+int _isupper(int c)
+{
+	if (c >= 0 && c <= 9)
+	{
+
+		return (1);
+
+	}
+	else
+	{
+
+		return (0);
+
+	}
+}
