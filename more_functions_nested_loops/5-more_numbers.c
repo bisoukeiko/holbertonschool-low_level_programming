e93f8c60eccf13f20a5d2876af0194d34689e956
bisoukeiko <87bisou@gmail.com>
2024-06-24 12:08:37 +0200

    5-more_numbers.c up2

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -15,8 +15,12 @@ void more_numbers(void)
 	{
 		for (num = 0; num <= 14; num++)
 		{
+			if (num <=10)
+			{
+				_putchar((num / 10) + '0');
+			}
 
-			_putchar(num + '0');
+			_putchar((num % 10) + '0');
 
 		}
 
@@ -24,6 +28,5 @@ void more_numbers(void)
 
 	}
 
-	_putchar('\n');
 
 }
