3e4eddb947d3f6479ebdf638c41ef43e239c6eef
bisoukeiko <87bisou@gmail.com>
2024-06-24 12:24:24 +0200

    6-print_line.c main.h add

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_line - draw a straight line in the terminal
+ * Return: nothing
+ */
+
+void print_line(int n)
+{
+	int cnt;
+
+	if (n <= 0)
+	{
+
+		for (cnt = 0; cnt < n; cnt++)
+		{
+			_putchar('_');
+		}
+
+	}
+
+	_putchar('\n');
+
+}
