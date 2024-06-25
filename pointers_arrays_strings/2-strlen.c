7d81ce1cc43f21370e4cf49aa78aa4109f2b2ae5
bisoukeiko <87bisou@gmail.com>
2024-06-25 13:35:09 +0200

    2-strlen.c main.h add

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - return the length of a string
+ * @s: pointer of string
+ * Return: length of a string
+ */
+
+int _strlen(char *s)
+{
+
+	char str[] = *s;
+	int cnt;
+
+	for (cnt = 0; str[cnt] != '\0'; ++cnt)
+	
+	return cnt;
+
+}
