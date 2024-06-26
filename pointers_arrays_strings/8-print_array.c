c509393f93518ea1224609dc19cb2e046e3f36c7
bisoukeiko <87bisou@gmail.com>
2024-06-26 11:35:27 +0200

    8-print_array.c main.h add

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_array - print n elements of an array of integers
+ *               followed by a new line
+ * @a: pointer of the elements of the array
+ * @n: number of elements of the array
+ * Return: nothing
+ */
+
+void print_array(int *a, int n)
+{
+
+	int index;
+
+	for (index = 0; index < n; index++)
+	{
+
+		printf("%d", a[index]);
+		printf(", ");
+
+
+	}
+
+	printf("\n");
+
+}
