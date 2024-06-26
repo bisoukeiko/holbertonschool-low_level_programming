23d124981a8907ba0a305f5f4597a40427c02982
bisoukeiko <87bisou@gmail.com>
2024-06-26 10:47:05 +0200

    7-puts_half.c up

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -22,7 +22,7 @@ void puts_half(char *str)
 	if ((len / 2) == 0)
 	{
 
-		for (index = (len / 2); index < len; index++)
+		for (index = (len / 2 + 1); index < len; index++)
 		{
 	
 			_putchar(str[index]);
@@ -33,7 +33,7 @@ void puts_half(char *str)
 	else
 	{
 
-		for (index = ((len - 1) / 2); index < len; index++)
+		for (index = (len / 2); index < len; index++)
 		{
 	
 			_putchar(str[index]);
