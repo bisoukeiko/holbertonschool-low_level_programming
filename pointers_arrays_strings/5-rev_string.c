a1dd25ece9d1244e5e8812e0ceedba8ef32db24a
bisoukeiko <87bisou@gmail.com>
2024-06-26 09:36:05 +0200

    5-rev_string.c main.h add

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * rev_string -  reverse a string
+ * @s: pointer of string
+ * Return: nothing
+ */
+
+void rev_string(char *s)
+{
+
+	int len;
+	int index1;
+	int index2 = 0;
+	char tmp;
+
+	for (len = 0; s[len] != '\0'; ++len)
+	{
+		continue;
+	}
+
+	for (index1 = (len -1); index1 > ((len-1) / 2 ); index1--)
+	{
+
+		tmp = s[index2];
+		s[index2] = s[index1];
+		s[index1] = tmp;
+
+		index2++;
+
+	}
+
+	s[len] = "\0";
+	
+
+}
