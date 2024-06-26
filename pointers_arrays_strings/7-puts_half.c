c21ce4d2e368681ff43479ad2abdaa7c99479653
bisoukeiko <87bisou@gmail.com>
2024-06-26 10:44:02 +0200

    7-puts_half.c main.h add

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ * puts_halh -  print the second half of a string
+ *              followed by a new line
+ *              
+ * @str: pointer of string
+ * Return: nothing
+ */
+
+void puts_half(char *str)
+{
+
+	int len;
+	int index;
+
+	for (len = 0; str[len] != '\0'; len++)
+	{
+		continue;
+	}
+
+	if ((len / 2) == 0)
+	{
+
+		for (index = (len / 2); index < len; index++)
+		{
+	
+			_putchar(str[index]);
+	
+		}
+
+	}
+	else
+	{
+
+		for (index = ((len - 1) / 2); index < len; index++)
+		{
+	
+			_putchar(str[index]);
+	
+		}
+
+	}
+
+	_putchar('\n');
+
+}
