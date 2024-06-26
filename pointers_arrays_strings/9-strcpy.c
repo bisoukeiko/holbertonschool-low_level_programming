a453c1b6cc98d3f5cedf358a925268ff7700472e
bisoukeiko <87bisou@gmail.com>
2024-06-26 13:41:56 +0200

    9-strcpy.c up

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -14,9 +14,9 @@ char *_strcpy(char *dest, char *src)
 
 	int index;
 
-	for (index = 0; str[index] != '\0'; index++)
+	for (index = 0; src[index] != '\0'; index++)
 	{
-		dest[index] = str[index];
+		dest[index] = src[index];
 	}
 
 	dest[index] = '\0';
