4fc8cbc1c9e304d1852c2f102650d5f2732e1f5e
bisoukeiko <87bisou@gmail.com>
2024-06-28 11:47:19 +0200

    1-strncat.c up

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -19,12 +19,14 @@ char *_strncat(char *dest, char *src, int n)
 		continue;
 	}
 
-	for (index_s = 0; index_s < n; index_s++)
+	for (index_s = 0; index_s < n && src[index_s] != '\0';
+		       index_s++, index_d++)
 	{
-		dest[index_d + index_s] = src[index_s];
+		dest[index_d] = src[index_s];
 	}
 
-	dest[index_d + index_s + 1] = '\0';
+	dest[index_d] = '\0';
+
 
 	return (dest);
 
