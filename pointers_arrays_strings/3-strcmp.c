152abf9833cdba5c7ab3b116d857a9903b000ca0
bisoukeiko <87bisou@gmail.com>
2024-06-28 14:05:10 +0200

    3-strcmp.c main.h add

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strcmp - cmpare two strings
+ * @dest: string to compare
+ * @src: string to compare
+ * Return: value s1 - s2
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+
+	while(*s1 && *s2)
+	{
+		if (*s1 - *s2 == 0)
+		{
+			return (0);
+		}
+	
+		s1++;
+		s2++;
+		
+	}
+
+	return (*s1 - *s2);
+
+}
