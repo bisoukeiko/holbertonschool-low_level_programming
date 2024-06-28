eb354d8b0d6b103165ec92a02f02c0491128978f
bisoukeiko <87bisou@gmail.com>
2024-06-28 12:46:57 +0200

    2-strncpy.c main.h up

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -14,19 +14,14 @@ char *_strncpy(char *dest, char *src, int n)
 	int index_d;
 	int index_s;
 
-	for (index_d = 0; dest[index_d] != '\0'; index_d++)
-	{
-		continue;
-	}
-
-	for (index_s = 0; index_s < n && src[index_s] != '\0';
-		       index_s++, index_d++)
+	for (index_s = 0, index_d = 0;
+	     index_s < n || src[index_s] != '\0';
+	     index_s++, index_d++)
 	{
 		dest[index_d] = src[index_s];
 	}
 
 	dest[index_d] = '\0';
-
 
 	return (dest);
 
