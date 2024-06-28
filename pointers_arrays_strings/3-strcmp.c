a2ace42de5b7cd77adffc434bca7e59c96fa0f91
bisoukeiko <87bisou@gmail.com>
2024-06-28 14:09:29 +0200

    3-strcmp.c up

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -12,9 +12,9 @@ int _strcmp(char *s1, char *s2)
 
 	while(*s1 && *s2)
 	{
-		if (*s1 - *s2 == 0)
+		if (*s1 - *s2 != 0)
 		{
-			return (0);
+			return (*s1 - *s2);
 		}
 	
 		s1++;
@@ -22,6 +22,6 @@ int _strcmp(char *s1, char *s2)
 		
 	}
 
-	return (*s1 - *s2);
+	return (0);
 
 }
