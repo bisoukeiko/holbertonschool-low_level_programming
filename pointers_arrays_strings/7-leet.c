311a7744652434d1744668b949201fd48983bd63
bisoukeiko <87bisou@gmail.com>
2024-06-30 08:07:38 +0200

    7-leet.c up

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -10,8 +10,8 @@ char *leet(char *str)
 {
 	int index_s;
 	int index_l;
-	char leet[10] = {'a','A','e','E','o','O','t','T','l','L'};
-	char code[10] = {'4','4','3','3','0','0','7','7','1','1'};
+	char leet[10] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+	char code[10] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
 
 	for (index_s = 0; str[index_s] != '\0'; index_s++)
 	{
