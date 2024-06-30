496c8d660e1305144c41b62d527226707ea6adbc
bisoukeiko <87bisou@gmail.com>
2024-06-30 06:50:54 +0200

    5-string_toupper.c main.h add

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * string_toupper - change all lowercase letters of a string
+ *                  to uppercase 
+ * @str: the string which include lowercase lettre 
+ * Return: pointer
+ */
+
+char *string_toupper(char *str)
+{
+	int index;
+
+	for (index = 0; str[index] != '\0'; index++)
+	{
+
+		if (str[index] >= 'a' && str[index] <= 'z')
+		{
+
+			str[inde] = str[index] -32;
+
+		}
+
+	}
+
+	return (str);
+
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -33,3 +33,5 @@ int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 
 void reverse_array(int *a, int n);
+
+char *string_toupper(char *);
