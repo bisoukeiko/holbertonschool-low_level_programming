60cd5b09f0e16df48e33ebcaf3892cab43a240c6
bisoukeiko <87bisou@gmail.com>
2024-06-30 06:22:08 +0200

    4-rev_array.c main.h add

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * reverse_array -  reverse the content of an array of integers
+ * @a: the array of integers to be reversed
+ * @n: the number of elements of the array
+ * Return: nothing
+ */
+
+int reverse_array(int *a, int n)
+{
+	int index;
+	int tmp;
+
+	n = n - 1;
+
+	for (index = 0; index <= n / 2; index++)
+	{
+		tmp = a[index];
+		a[index = a[n];
+		a[n] = tmp;
+		n--;
+	}
+
+}
