3e2701420fe828ce3c2ddae3be0777ce38eb14ce
bisoukeiko <87bisou@gmail.com>
2024-07-03 14:20:02 +0200

    1-print_rev_recursion.c add

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -10,12 +10,11 @@
 void _print_rev_recursion(char *s)
 {
 
-	if (*s == '\0')
+	if (*s != '\0')
 	{
-		return;
-	}
+		_print_rev_recursion(s + 1);
 
-	_print_rev_recursion(s + 1);
-	
-	_putchar(*s);
+		_putchar(*s);
+
+	}
 }
