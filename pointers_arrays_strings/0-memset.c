c31f1cc6e7de885d5957bd8e2220fb9554a9256f
bisoukeiko <87bisou@gmail.com>
2024-07-04 09:06:35 +0200

    0-memset.c up

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * memset - fills memory with a constant byte
+ * _memset - fills memory with a constant byte
  * @s: memory area
  * @b: constant byte
  * @n: bytes filled
