06c48cc1649fa9c16c2f0c6d835beceb981e3f2a
bisoukeiko <87bisou@gmail.com>
2024-07-04 11:40:04 +0200

    3-mul.c add

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiplie two numbers
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0
+ *       : 1 If the program does not receive two arguments
+ */
+
+int main(int argc, char *argv[])
+{
+
+	if (argc < 3)
+	{
+		printf("Error");
+		return (1);
+
+	}
+	else
+	{
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	}
+
+	return (0);
+
+}
