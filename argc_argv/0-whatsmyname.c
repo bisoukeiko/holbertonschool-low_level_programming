4593e3f7249f484871311bafcd6f62e139f72442
bisoukeiko <87bisou@gmail.com>
2024-07-04 10:51:41 +0200

    0-whatsmyname.c up

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,12 +1,17 @@
-#include<stdio.h>
+#include <stdio.h>
 
 /**
  * main - print program name 
  * Return: nothing
  */
 
-void main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-	printf(argv[0])};
+	if (argc >= 1)
+	{
+		printf("%s\n", argv[0]);
+	}
+
+	return (0);
 
 }
