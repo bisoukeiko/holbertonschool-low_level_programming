b7814f28221afdd8224de1a4e66b1167b4980d56
bisoukeiko <87bisou@gmail.com>
2024-07-05 15:43:12 +0200

    100-is_palindrome.c

--- recursion/100-is_palindrome.c
+++ recursion/100-is_palindrome.c
@@ -11,14 +11,14 @@
 
 int check_palindrome(char *s, int len, int index)
 {
-	if (index == len - index || index == len - index -1)
+	if (index == len - index || index == len - index - 1)
 	{
 		return (1);
 	}
 
 	if (*(s + index) == *(s + (len - index - 1)))
 	{
-		return(check_palindrome(s, len, index + 1));
+		return (check_palindrome(s, len, index + 1));
 	}
 
 	return (0);
