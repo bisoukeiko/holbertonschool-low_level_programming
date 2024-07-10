d1850528f3a50156f5e304fce62e3fb63a3ab854
bisoukeiko <87bisou@gmail.com>
2024-07-10 13:51:48 +0200

    dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,15 @@
 #ifndef _DOG_H_
 #define _DOG_H_
 
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+void print_dog(struct dog *d);
+
+dog_t *new_dog(char *name, float age, char *owner);
+
+void free_dog(dog_t *d);
+
+
 /**
  * struct dog - data of dog
  * @name: name
