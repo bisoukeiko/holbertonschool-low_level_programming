4de6d5a96f1b2ddee9d99cbbe5790b5a3c7444b3
bisoukeiko <87bisou@gmail.com>
2024-07-10 15:46:17 +0200

    dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -16,6 +16,15 @@ struct dog
 };
 
 
+
+/**
+ * dog_t - Typedef for dog
+ */
+
+typedef struct dog dog_t;
+
+
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
 void print_dog(struct dog *d);
