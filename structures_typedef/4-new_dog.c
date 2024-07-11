d33d87ae996938b023e7abb17982eea22836a2e0
bisoukeiko <87bisou@gmail.com>
2024-07-11 11:38:49 +0200

    4-new_dog.c dog.h

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,36 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * new_dog - create a new dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ * Return: pointer of struct
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *ptr;
+
+	if (name == NULL || age < 0 || owner == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		ptr = malloc(sizeof(ptr));
+
+		if (ptr == NULL)
+		{
+			return (NULL);
+		}
+
+		ptr->name = name;
+		ptr->age = age;
+		ptr->owner = owner;
+	}
+
+	return (ptr);
+
+}
--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -27,5 +27,7 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 
 void print_dog(struct dog *d);
 
+dog_t *new_dog(char *name, float age, char *owner);
+
 
 #endif
