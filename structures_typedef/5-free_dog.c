424948890413dceeeb31ab9f0837cd021ec627cc
bisoukeiko <87bisou@gmail.com>
2024-07-12 00:19:48 +0200

    5-free_dog.c dog.h

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,28 @@
+#include "dog.h"
+#include <stddef.h>
+
+/**
+ * free_dog - frees dog
+ * @d: pointer
+ * Return: nothing
+ */
+
+void free_dog(dog_t *d)
+{
+
+	if (d != NULL)
+	{
+		if (d->name != NULL)
+		{
+			free(d->name);
+		}
+
+		if (d->owner != NULL)
+		{
+			free(d->owner);
+		}
+
+		free(d);
+	}
+
+}
--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -29,5 +29,7 @@ void print_dog(struct dog *d);
 
 dog_t *new_dog(char *name, float age, char *owner);
 
+void free_dog(dog_t *d);
+
 
 #endif
