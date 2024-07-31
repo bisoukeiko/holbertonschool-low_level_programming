aebc29f34824836ddd1b6a36a890d5a6a8b10b30
bisoukeiko <87bisou@gmail.com>
2024-07-31 14:29:16 +0200

    0-print_list.c up specifir

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -16,12 +16,12 @@ size_t print_list(const list_t *h)
 	{
 		if (h->str == NULL)
 		{
-			printf("[%d] %s\n", 0, "(nil)");
+			printf("[0] (nil)\n");
 
 		}
 		else
 		{
-			printf("[%u] %s\n", h->len, h->str);
+			printf("[%d] %s\n", h->len, h->str);
 
 		}
 
