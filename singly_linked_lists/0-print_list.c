740d7a85bdfb2b3053babe82786f891555622abc
bisoukeiko <87bisou@gmail.com>
2024-07-31 12:49:35 +0200

    0-print_list.c up2

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -4,15 +4,15 @@
 /**
  * print_list - print all the elements of a list_t list
  * @h: The list_t list
- * Return: The number of nodes in h
+ * Return: The number of nodes in the list
  */
 
 size_t print_list(const list_t *h)
 {
 
-	int node = 0;
+	size_t node = 0;
 
-	while (h != NULL)
+	while (h)
 	{
 		if (h->str == NULL)
 		{
