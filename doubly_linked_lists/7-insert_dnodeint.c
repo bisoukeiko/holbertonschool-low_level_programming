606f4e594b343755b43c543ed54c0476b307a4db
bisoukeiko <87bisou@gmail.com>
2024-08-02 07:49:02 +0200

    7-insert_dnodeint.c up

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -37,7 +37,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 					new = malloc(sizeof(dlistint_t));
 					if (new == NULL)
 					{
-						return (NULL)
+						return (NULL);
 					}
 					new->n = n;
 					new->next = head->next;
