95a3085759020062ea7c9662ecd74e12a1ec3327
bisoukeiko <87bisou@gmail.com>
2024-08-02 07:15:32 +0200

    7-insert_dnodeint.c up

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -25,7 +25,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	else
 	{
 		head = *h;
-		while (head->prev != NULL)
+		while (head != NULL)
 		{
 			head = head->prev;
 		}
