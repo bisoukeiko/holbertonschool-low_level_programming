48fe0d9a3179a54c34c07914ceb408267efb9c1c
bisoukeiko <87bisou@gmail.com>
2024-08-02 07:43:27 +0200

    7-insert_dnodeint.c up

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -2,19 +2,19 @@
 #include "lists.h"
 
 /**
- * insert_dnodeint_at_index - inserts a new node at
- * a given position
- *
- * @h: head of the list
- * @idx: index of the new node
- * @n: value of the new node
- * Return: the address of the new node, or NULL if it failed
+ * insert_dnodeint_at_index - Insert a new node at a given position
+ * @h: The pointer to the address of the head of list_t list
+ * @idx : The index of the list where the new node should be added
+ * @n : The integer
+ * Return: The address of the new node
+ *         NULL if the program filed
  */
+
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *new;
 	dlistint_t *head;
-	unsigned int i;
+	unsigned int index;
 
 	new = NULL;
 	if (idx == 0)
@@ -22,13 +22,13 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	else
 	{
 		head = *h;
-		i = 1;
+		index = 1;
 		if (head != NULL)
 			while (head->prev != NULL)
 				head = head->prev;
 		while (head != NULL)
 		{
-			if (i == idx)
+			if (index == idx)
 			{
 				if (head->next == NULL)
 					new = add_dnodeint_end(h, n);
@@ -47,7 +47,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 				break;
 			}
 			head = head->next;
-			i++;
+			index++;
 		}
 	}
 
