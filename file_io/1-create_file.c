2cfaba10926c57b1bb82ad1ad80e54eb48e6bea4
bisoukeiko <87bisou@gmail.com>
2024-08-03 06:42:28 +0200

    1-create_file.c up

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -2,39 +2,33 @@
 #include "main.h"
 
 /**
- * create_file - Create a file
- * @filename: The pointer to the file to create
- * @text_content: a NULL terminated string to write to the file
- * Return: 1 on success, -1 on failure
+ * create_file - Creates a file.
+ * @filename: A pointer to the name of the file to create.
+ * @text_content: A pointer to a string to write to the file.
+ *
+ * Return: If the function fails - -1.
+ *         Otherwise - 1.
  */
-
 int create_file(const char *filename, char *text_content)
 {
-	ssize_t fd, fwrite;
-	int len;
+	int o, w, len = 0;
 
 	if (filename == NULL)
-	{
 		return (-1);
-	}
 
 	if (text_content != NULL)
 	{
-		for (len = 0; text_content[len] != '\0'; len++)
-		{
-			continue;
-		}
+		for (len = 0; text_content[len];)
+			len++;
 	}
 
-	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
-	fwrite = write(fd, text_content, len);
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(o, text_content, len);
 
-	if (fd == -1 || fwrite == -1)
-	{
+	if (o == -1 || w == -1)
 		return (-1);
-	}
 
-	close(fd);
+	close(o);
 
 	return (1);
 }
