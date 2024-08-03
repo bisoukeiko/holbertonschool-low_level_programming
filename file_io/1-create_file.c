fd5696ed711274b21da048a993504a2016382d1d
bisoukeiko <87bisou@gmail.com>
2024-08-03 06:56:31 +0200

    1-create_file.c up

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -1,13 +1,12 @@
-#include <stdlib.h>
 #include "main.h"
 
 /**
- * create_file - Create a file
- * @filename: The pointer to the file to create
- * @text_content: a NULL terminated string to write to the file
+ * create_file - Creates a file
+ * @filename: A pointer to the name of the file to create
+ * @text_content: A pointer to a string to write to the file
  * Return: 1 on success, -1 on failure
+ *         - 1, if filename is NULL
  */
-
 int create_file(const char *filename, char *text_content)
 {
 	ssize_t fd, fwrite;
@@ -20,19 +19,17 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 	{
-		for (len = 0; text_content[len]; len++)
+		for (len = 0; text_content[len];)
 		{
-			continue;
+			len++;
 		}
 	}
 
-	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	fwrite = write(fd, text_content, len);
 
 	if (fd == -1 || fwrite == -1)
-	{
 		return (-1);
-	}
 
 	close(fd);
 
