94caf7fbb5195d85417239e4affe7b06b4faef1a
bisoukeiko <87bisou@gmail.com>
2024-08-09 06:42:53 +0200

    task3: fix error

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -50,7 +50,7 @@ void func_err(int code, char *file, int fd)
 void func_copy(char *file_from, char *file_to)
 {
 	int fd_from, fd_to, fread, fwrite, fclose;
-	char *buffer[MAXSIZE];
+	char *buffer[1024];
 
 	fd_from = open(file_from, O_RDONLY);
 	if (fd_from == -1)
@@ -62,10 +62,10 @@ void func_copy(char *file_from, char *file_to)
 	if (fd_to == -1)
 		func_err(99, file_to, 0);
 
-	fread = MAXSIZE;
-	while (fread == MAXSIZE)
+	fread = 1024;
+	while (fread == 1024)
 	{
-		fread = read(fd_from, buffer, MAXSIZE);
+		fread = read(fd_from, buffer, 1024);
 		if (fread == -1)
 			func_err(98, file_from, 0);
 
@@ -73,9 +73,6 @@ void func_copy(char *file_from, char *file_to)
 		if (fwrite == -1 || fread != fwrite)
 			func_err(99, file_to, 0);
 	}
-
-	if (fread == -1)
-		func_err(98, file_from, 0);
 
 	fclose = close(fd_from);
 	if (fclose == -1)
